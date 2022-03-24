3e28ef591e86cb8ffbeb623a0e33aeb3ae092f31
JoanaCoelhoAlves <100436883+JoanaCoelhoAlves@users.noreply.github.com>
2022-03-24 11:29:20 +0000

    Update interface_sql2pandas.py

--- interface_sql2pandas.py
+++ interface_sql2pandas.py
@@ -18,7 +18,7 @@ import getpass
 import seaborn as sn
 from matplotlib import pyplot as plt
 
-sn.set_palette(palette= "husl")
+sn.set_palette(palette= "pastel")
 
 links = ["<a href='#logistic'>Logistic</a>",\
          "<a href='#finances-1'>Finances 1</a>",\
