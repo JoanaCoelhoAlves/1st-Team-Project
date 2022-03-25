0a3c68e87c5905dd2e1eed63d3d3aebc09839298
JoanaCoelhoAlves <100436883+JoanaCoelhoAlves@users.noreply.github.com>
2022-03-25 14:16:13 +0000

    changed pallete colour

--- interface_sql2pandas.py
+++ interface_sql2pandas.py
@@ -18,7 +18,7 @@ import getpass
 import seaborn as sn
 from matplotlib import pyplot as plt
 
-sn.set_palette(palette= "Dark 2")
+sn.set_palette(palette= "Dark2")
 
 links = ["<a href='#Logistic'>Logistic</a>",\
          "<a href='#Finances'>Finances </a>",\
